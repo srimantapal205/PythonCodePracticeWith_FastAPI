ceb282370e07f6baeac607a2c3c04326c80c6e7e
Srimanta Pal <srimantapal205@hotmail.com>
2025-07-13 21:02:44 +0530

    Dictionari completed

--- BasicPython/dictionary/dictionary.py
+++ BasicPython/dictionary/dictionary.py
@@ -33,6 +33,64 @@ for x in user_dictionary:
 for x, y in user_dictionary.items():
     print(f'{x} : {y}')
 
+user_dictionary2 =user_dictionary.copy()
+user_dictionary2.pop('age')
+print(user_dictionary)
+print(user_dictionary2)
 
-# ShaloCopy
-user_dictionary =us
+
+
+
+user_dictionary2 =user_dictionary
+user_dictionary2.pop('age')
+print(user_dictionary)
+
+print('----------------------------------------------------')
+
+"""
+Based on the dictionary:
+
+my_vehicle = {
+    "model": "Ford",
+    "make": "Explorer",
+    "year": 2018,
+    "mileage": 40000
+}
+- Create a for loop to print all keys and values
+
+- Create a new variable vehicle2, which is a copy of my_vehicle
+
+- Add a new key 'number_of_tires' to the vehicle2 variable that is equal to 4
+
+- Delete the mileage key and value from vehicle2
+
+- Print just the keys from vehicle2
+
+"""
+my_vehicle = {
+    "model": "Ford",
+    "make": "Explorer",
+    "year": 2018,
+    "mileage": 40000
+}
+
+print('-----------Create a for loop to print all keys and values-----------------\n')
+for key, value in my_vehicle.items():
+    print(f'{key} : {value}')
+print('----------------------Create a new variable vehicle2, which is a copy of my_vehicle----------------------\n')
+vehicle2 = my_vehicle.copy()
+print(vehicle2)
+
+print('-----Add a new key "number_of_tires" to the vehicle2 variable that is equal to 4----\n')
+vehicle2['number_of_tires'] = 4
+print(vehicle2)
+
+print('-------------------------Delete the mileage key and value from vehicle2---------------------------\n')
+vehicle2.pop('mileage')
+print(vehicle2)
+
+print('------------------------- Print just the keys from vehicle2---------------------------\n')
+
+
+for key in vehicle2.keys():
+    print(f'Key : {key}')
