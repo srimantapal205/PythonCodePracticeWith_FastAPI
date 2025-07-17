e090326b02434b0d5d9a14aefac993e2cb992bf2
Srimanta Pal <srimantapal205@hotmail.com>
2025-07-17 22:31:37 +0530

    Update Encapsulation

--- OopsConcept/Enemy.py
+++ OopsConcept/Enemy.py
@@ -1,18 +1,29 @@
 class Enemy :
-    #Encapsulation
-    type_of_enemy: str
-    health_points: int = 10
-    attack_damage: int = 2
 
+    # type_of_enemy: str
+    # health_points: int = 10
+    # attack_damage: int = 2
+
+    #
     # Abstraction
+
+    def  __init__(self,type_of_enemy, health_points, attack_damage):
+        self.__type_of_enemy = type_of_enemy
+        self.health_points = health_points
+        self.attack_damage = attack_damage
+
     def talk(self) :
-        print(f'{self.type_of_enemy}. Be prepared to fight!')
+        print(f'{self.__type_of_enemy}. Be prepared to fight!')
 
     def walk_forward(self):
-        print(f'{self.type_of_enemy} moves closer to you!')
+        print(f'{self.__type_of_enemy} moves closer to you!')
 
     def attack(self):
-        print(f'{self.type_of_enemy} attacks you for {self.attack_damage} damage!')
+        print(f'{self.__type_of_enemy} attacks you for {self.attack_damage} damage!')
+
+    # Encapsulation
+    def get_type_of_enemy(self):
+        return  self.__type_of_enemy
 
     # Inheritance
 
--- OopsConcept/Main.py
+++ OopsConcept/Main.py
@@ -1,24 +1,31 @@
 from Enemy import *
-enemy = Enemy()
+enemy = Enemy('Goblin', 100, 10)
+# print(enemy.health_points)
+#
+# enemy_1 = Enemy()
+# print(enemy_1.health_points)
+#
+# enemy_2 = Enemy()
+# enemy_2.health_points = 200
+# print(enemy_2.health_points)
+#
+# enemy.type_of_enemy = 'Orc'
+#
+# print(f'{enemy.type_of_enemy} has {enemy.health_points} health points and can do an attack of {enemy.attack_damage} '
+#       )
+#
+# enemy.type_of_enemy = 'Goblin'
+#
+# print(enemy.talk())
+# print(enemy.walk_forward())
+# print(enemy.attack())
+
+
 print(enemy.health_points)
-
-enemy_1 = Enemy()
-print(enemy_1.health_points)
-
-enemy_2 = Enemy()
-enemy_2.health_points = 200
-print(enemy_2.health_points)
-
-enemy.type_of_enemy = 'Orc'
-
-print(f'{enemy.type_of_enemy} has {enemy.health_points} health points and can do an attack of {enemy.attack_damage} '
-      )
-
-enemy.type_of_enemy = 'Goblin'
-
 print(enemy.talk())
-print(enemy.walk_forward())
 print(enemy.attack())
+print(enemy.get_type_of_enemy())
+
 
 
 
